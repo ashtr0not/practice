8cbb36f1f8cde1af89235177c0c45c6f573e744c
Ash Carey DEV <ashtr0not21@gmail.com>
2025-04-11 17:52:33 -0700

    server file

--- server.js
+++ server.js
@@ -1,42 +1,67 @@
-const express = require('express') 
-const app = express() //this gives us access to all the functions that comes with express 
-const cors = require('cors') // This is a cors module that gets rid of an cors errors for us
-const PORT = 8000
+//Setting up Required Modules
+const express = require('express') // we are requiring express and we will consume it with code in line 2
+const app = express() //wherever you see app, this is really just express 
+const MongoClient = require('mongodb').MongoClient //this package helps us connect to our database 
+const PORT = 2121 // this is setting our port 
+//Setting up Required Modules 
 
-app.use(cors())
+//Database Set Up
+let db, // this is a global variable
+    dbConnectionStr = 'mongodb+srv://ashcarey0421:Neli-Ash-and-Kaleo@testcluster.ydsl9q8.mongodb.net/', // this connection string is given by Mongo Atlas that is given when creating a cluster
+    dbName = 'rap' // this is the name of the actual database 
 
-let workout = { //AKA Express API
-    'arm': { // This is my own API I had created. This API can be seen in post man under localhost:8000/api/workout 
-        muscleGroup : ['bicep', ' forearm', ' triceps'],
-        exercise : ['Bicep Curl', 'Pull-up', 'Chin-up', 'Concentration curl', 'Bent-over row'],
-        description : 'Biceps are the large muscles located in your upper arm, extending from the shoulder to just above the elbow'
-    },
-    'leg': {
-        muscleGroup : ['quads', 'calves', 'hamstrings'],
-        exercise : ['Leg Extensions', 'Calf Raises', 'Squats'],
-        description : 'The quads are a group of five muscles located in your upper leg at the fron of your thigh.'
-    },'unknown': {
-        muscleGroup : ['unknown','unknown'],
-        exercise : 'unknownwww',
-        description : 'I am unknown'
-    }
-}
+MongoClient.connect(dbConnectionStr) // this connects our application to our db. 
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName) // This is a global vairable that stores the database that is connected
+    })
+//Database Set Up 
 
+// Middleware/Application Set Up
+app.set('view engine', 'ejs') // telling this to use the templating language ejs 
+app.use(express.static('public')) // telling this to serve up any files placed in the public folder 
+app.use(express.urlencoded({extended: true})) // use this code to parse or look at any of the requests that are coming in
+app.use(express.json()) // this line and the one above enable us to look at the data that was sent with the form 
+// Middleware/Application Set Up
 
-app.get('/', (request, response) => { // the '/' is where the home page or root is loaded. Once this is ran, a call back function is called(request, response)
-    response.sendFile(__dirname + '/index.html') // once the get function is ran, we want to respond with some HTML and it looks for the HTML wherever the server file is located
-}) // get takes in two values 
+//This whole section makes up our API
 
-app.get('/api/workout/:bodyPart', (request,response) => {
-    const bodyPart = request.params.bodyPart.toLowerCase() // request: look at the request params: look at the parameter bodyPart bodyPart: grab the parameter in the URL toLowerCase(): gets rid of case sensitivity 
-    if(workout[bodyPart]){ // if conditional to default to unknown if name isn't found 
-    response.json(workout[bodyPart]) // pulling from const bodyPart above in line 31
-    //localhost:8000/api/workout/arm  will pull from the arm object in the API above 
-    }else{
-        response.json(workout['unknown']) // this defaults to unknown the the query parameter isnt met 
-    }
+// GET Request 
+app.get('/', (request, response) => {
+    db.collection('rappers').find().toArray()
+    .then(data => { // data is the array that holds the objects 
+        response.render('index.ejs', {info: data}) //we are assigning the data to the cariable info which is used in our EJS file
+        //the render is what spits out html from that ejs file 
+    })
+    .catch(error => console.error(error))
 })
+// GET Request 
 
-app.listen(PORT, () => {
+//POST Request
+app.post('/addRapper', (request,response) => {
+    db.collection('rappers').insertOne(request.body) //the items inputted into the form is not the request.body
+    .then(result =>{
+        console.log('Rapper Added')
+        response.redirect('/')
+    })
+})
+//POST Request
+
+//DELETE Request
+
+app.delete('/deleteRapper', (request,response) => {
+    db.collection('rappers').deleteOne({stageName: request.body.stageNameS})
+    .then(result => {
+        console.log('Rapper Deleted')
+        response.json('Rapper Deleted')
+    })
+    .catch(error => console.error(error))
+})
+//This whole section makes up our API
+
+//Setting up the app to listen on the port set on line 5
+app.listen(process.env.PORT || PORT, ()=> {
     console.log(`Server running on port ${PORT}`)
-})//another method that comes with Express
+})
+//If she up with Heroku we will use whatever port Heroku assigns 
+
